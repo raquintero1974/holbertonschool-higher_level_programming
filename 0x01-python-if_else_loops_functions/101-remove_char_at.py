6ee1901e3f26782e05e011f8442550a761ddeb79
Raul Quintero <4638@holbertonschool.com>
2022-05-04 23:47:58 -0500

    Fixing code

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-
 def remove_char_at(str, n):
-    """ Copy a string and remove a character at n """
-    return str[:n] + str[(len(str) + n) % len(str) + 1:]
+    empty = ""
+    for x, y in enumerate(str):
+        if x != n:
+            empty += y
+    return empty
