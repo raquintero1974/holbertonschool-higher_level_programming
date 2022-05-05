39b9303178dc95302405bab73198426c76898138
Raul Quintero <4638@holbertonschool.com>
2022-05-04 23:37:13 -0500

    Fixing code

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-"Prints numbers from 0 to 98 in hexadecimal"
-for i in range(0, 99):
-    print("{:d} = 0x{:x}".format(i, i))
+print(*("{} = {:#x}".format(i, i) for i in range(99)), sep='\n')
