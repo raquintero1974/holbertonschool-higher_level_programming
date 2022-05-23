63925cfd2afe316884ac739dfb6aadf6daaf5e79
Raul Quintero <4638@holbertonschool.com>
2022-05-23 18:58:58 -0500

    Adding files

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+from add_0 import add
 
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+def main():
+    a = 1
+    b = 2
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    main()
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+from add_0 import add
 
-if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
 
-    print(a)
+def main():
+    a = 1
+    b = 2
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    main()
