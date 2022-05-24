5411445ea8393333793e3b5349e4491af8780e99
Raul Quintero <4638@holbertonschool.com>
2022-05-23 19:23:37 -0500

    Fixing code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-
-
-def main():
-    a = 1
-    b = 2
-    print('{} + {} = {}'.format(a, b, add(a, b)))
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    import hidden_4
+    for func in dir(hidden_4):
+        if func[0] != '_' and func[1] != '_':
+            print(func)
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-from add_0 import add
+from variable_load_5 import a
 
-
-def main():
-    a = 1
-    b = 2
-    print('{} + {} = {}'.format(a, b, add(a, b)))
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    print("{:d}".format(a))
