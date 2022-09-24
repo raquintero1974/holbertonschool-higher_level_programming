5dae8d3d18ac57d4bf1ced7060315894400fdb02
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-23 21:47:14 -0500

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,6 @@
-f __name__ == "__main__":
-
-        from add_0 import add
-
-            a = 1
-
-                b = 2
-
-                    print("{:d} + {:d} = {:d}".format(a, b, add(a, b))):wq
-
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
