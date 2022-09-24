d47f47fa8e4eac75b28afb0fced0ce64d494d64a
raquintero1974 <4639@holbertonschool.com>
2022-09-23 18:30:13 -0700

    Adding file 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,17 @@
+n range(0, 10):
+
+        for y in range(0, 10):
+
+                    if x < y:
+
+                                    print('{}{}'.format(x, y), end="")
+
+                                                if x < 8:
+
+                                                                    print(', ', end="")
+
+                                                                    print('\n', end="")
+
+                                                                    © 2022 GitHub, Inc.
+
+
