320ee71a6a583badf2b0b9078749a69e54932dd8
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-23 20:31:41 -0500

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,17 +1,9 @@
-n range(0, 10):
-
-        for y in range(0, 10):
-
-                    if x < y:
-
-                                    print('{}{}'.format(x, y), end="")
-
-                                                if x < 8:
-
-                                                                    print(', ', end="")
-
-                                                                    print('\n', end="")
-
-                                                                    © 2022 GitHub, Inc.
-
-
+#!/usr/bin/python3
+for x in range(0, 10):
+    for y in range(0, 10):
+        if x < y:
+            print('{}{}'.format(x, y), end="")
+            if x < 8:
+                print(', ', end="")
+print('\n', end="")
+© 2022 GitHub, Inc.
