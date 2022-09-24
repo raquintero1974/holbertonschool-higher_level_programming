e6a85e6a95cd5ce229cab2b79fde12849405f07d
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-23 20:19:04 -0500

    Update 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,15 +1,8 @@
- in list(
-
-                 range(ord('a'), ord('e'))
-
-                         ) + list(
-
-                                                 range(ord('f'), ord('q'))
-
-                                                                 ) + list(
-
-                                                                                                 range(ord('r'), ord('z') + 1)
-
-                                                                                                                         ):
-
-                                                                         print('{:c}'.format(i), end="")
+#!/usr/bin/python3
+for i in list(
+        range(ord('a'), ord('e'))) + list(
+                range(ord('f'), ord('q'))
+                ) + list(
+                        range(ord('r'), ord('z') + 1)
+                        ):
+    print('{:c}'.format(i), end="")
