92ed02034d3cf601101f1044f8fdbaab7da9efc7
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-23 20:37:35 -0500

    Update 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
-slower(c):
-
-        """ Checks if a character is lowercase """
-
-            return (ord('a') <= ord(c) <= ord('z'))
+#!/usr/bin/python3
 
 
+def islower(c):
+    """ Checks if a character is lowercase """
+    return (ord('a') <= ord(c) <= ord('z'))
