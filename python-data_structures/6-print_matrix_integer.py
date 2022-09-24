06697ec6af71bf600d052840faf868ac7aef2b2e
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-23 22:39:59 -0500

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,8 @@
-x_integer(matrix=[[]]):
-
-        for x in matrix:
-
-                    for i in x:
-
-                                    print("{:d}".format(i), end=" " if x[-1] != i else "")
-
-                                            print()
+#!/usr/bin/python3
 
 
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for i in x:
+            print("{:d}".format(i), end=" " if x[-1] != i else "")
+        print()
