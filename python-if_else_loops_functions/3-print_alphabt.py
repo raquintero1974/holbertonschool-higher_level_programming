b7420dc9d257f5c8577aa1993f2a1646146a1204
raquintero1974 <4639@holbertonschool.com>
2022-09-23 18:16:07 -0700

    Adding file 3

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,15 @@
+ in list(
+
+                 range(ord('a'), ord('e'))
+
+                         ) + list(
+
+                                                 range(ord('f'), ord('q'))
+
+                                                                 ) + list(
+
+                                                                                                 range(ord('r'), ord('z') + 1)
+
+                                                                                                                         ):
+
+                                                                         print('{:c}'.format(i), end="")
