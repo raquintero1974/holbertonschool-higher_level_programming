494ec54705896dae65a2673ef803973f8afb5fbf
raquintero1974 <4639@holbertonschool.com>
2022-09-23 20:17:35 -0700

    Adding file 1

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,21 @@
+t(my_list, idx):
+
+        if idx < 0 or idx > len(my_list) - 1:
+
+                    return None
+
+                    else:
+
+                                return my_list[idx]
+
+t(my_list, idx):
+
+        if idx < 0 or idx > len(my_list) - 1:
+
+                    return None
+
+                    else:
+
+                                return my_list[idx]
+
+
