f628b987064456086701359d6977bbf1b294d9f0
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-23 22:01:53 -0500

    Update 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,6 @@
-f __name__ == "__main__":
-
-        import hidden_4
-
-            for func in dir(hidden_4):
-
-                        if func[0] != '_' and func[1] != '_':
-
-                                        print(func)
-
-
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    for func in dir(hidden_4):
+        if func[0] != '_' and func[1] != '_':
+            print(func)
