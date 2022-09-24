374df67635c59aacef9f088a820521746d79e1df
raquintero1974 <4639@holbertonschool.com>
2022-09-23 20:38:51 -0700

    Adding file 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+x_integer(matrix=[[]]):
+
+        for x in matrix:
+
+                    for i in x:
+
+                                    print("{:d}".format(i), end=" " if x[-1] != i else "")
+
+                                            print()
+
+
