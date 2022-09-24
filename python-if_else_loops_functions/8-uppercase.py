ed2b2187826b541dc77cdf45545a36e5307236bd
raquintero1974 <4639@holbertonschool.com>
2022-09-23 18:39:58 -0700

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+se(str):
+
+        """ Prints a string in uppercase (followed by a newline) """
+
+            print("{}".format(str.translate(
+
+                        {(c | 32): c for c in range(ord('A'), ord('Z') + 1)}
+
+                            )))
+
+
