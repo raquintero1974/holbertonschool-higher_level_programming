eb60cb5dad0b1e4a468dc1b3e545e9f6d7e8cab1
raquintero1974 <4639@holbertonschool.com>
2022-09-23 20:49:33 -0700

    Adding file 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,19 @@
+nteger(my_list=[]):
+
+        if len(my_list) == 0:
+
+                    return None
+
+
+
+                    max = my_list[0]
+
+                        for x in my_list[1:]:
+
+                                    if x > max:
+
+                                                    max = x
+
+                                                        return max
+
+
