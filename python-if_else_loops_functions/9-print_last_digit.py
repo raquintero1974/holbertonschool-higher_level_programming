3c9d1f46264d2b7e5f270dff1f404453750e0692
raquintero1974 <4639@holbertonschool.com>
2022-09-23 18:45:39 -0700

    Adding file 9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+st_digit(number):
+
+        """
+
+            Prints and returns the last digit of a number.
+
+                If given something that's not a number, this returns None.
+
+                    """
+
+                        number = (abs(number) % 10)
+
+                            print("{}".format(number), end="")
+
+                                return (int(number))
