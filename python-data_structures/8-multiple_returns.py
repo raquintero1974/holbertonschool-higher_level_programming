d662edb14d79649a378a03c3cbac8ed3465ea118
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-23 22:46:13 -0500

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
-(sentence):
-
-        if (len(sentence)) == 0:
-
-                    return len(sentence), None
-
-                    return len(sentence), sentence[0]
+#!/usr/bin/python3
 
 
+def multiple_returns(sentence):
+    if (len(sentence)) == 0:
+        return len(sentence), None
+    return len(sentence), sentence[0]
