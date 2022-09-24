7e1dbe62e81e6aefd3c36dd3aa5a6857a728fb93
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-23 20:41:19 -0500

    Update 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,7 @@
-se(str):
+#!/usr/bin/python3
 
-        """ Prints a string in uppercase (followed by a newline) """
-
-            print("{}".format(str.translate(
-
-                        {(c | 32): c for c in range(ord('A'), ord('Z') + 1)}
-
-                            )))
-
-
+def uppercase(str):
+    """ Prints a string in uppercase (followed by a newline) """
+    print("{}".format(str.translate(
+        {(c | 32): c for c in range(ord('A'), ord('Z') + 1)}
+    )))
