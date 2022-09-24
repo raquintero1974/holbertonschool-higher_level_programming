c79adde702c92e53e2d71d1be8c9a57579a281df
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-23 22:18:25 -0500

    Update 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,21 +1,6 @@
-t(my_list, idx):
-
-        if idx < 0 or idx > len(my_list) - 1:
-
-                    return None
-
-                    else:
-
-                                return my_list[idx]
-
-t(my_list, idx):
-
-        if idx < 0 or idx > len(my_list) - 1:
-
-                    return None
-
-                    else:
-
-                                return my_list[idx]
-
-
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
+    else:
+        return my_list[idx]
