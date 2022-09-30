62626b106c3f58da4a784025fa7801970169bedd
raquintero1974 <4639@holbertonschool.com>
2022-09-30 10:55:30 -0700

    Adding README and 0 file

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,19 @@
+afe_print_list(my_list=[], x=0):
+
+        count = 0
+
+            try:
+
+                        for i in range(x):
+
+                                        print("{}".format(my_list[i]), end="")
+
+                                                    count += 1
+
+                                                        except:
+
+                                                                    pass
+
+                                                                    print()
+
+                                                                        return count
