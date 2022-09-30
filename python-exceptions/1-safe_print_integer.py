231567bcf91c704bea667f9c6a4bf8da01970596
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-30 13:02:12 -0500

    Update 1-safe_print_integer.py
    Fixing code

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,7 @@
-afe_print_integer(value):
-
-        try:
-
-                    print("{:d}".format(value))
-
-                        except:
-
-                                    return False
-
-                                    return True
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except:
+        return False
+    return True
