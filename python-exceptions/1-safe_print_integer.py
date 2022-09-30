f2eb0c5d0dd1bc57db54b9ad95721e563bd52ef9
raquintero1974 <4639@holbertonschool.com>
2022-09-30 11:00:41 -0700

    Adding file 1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+afe_print_integer(value):
+
+        try:
+
+                    print("{:d}".format(value))
+
+                        except:
+
+                                    return False
+
+                                    return True
