0462a51b8b19cc2a2bffbceecc0546057dfab13c
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-30 09:18:46 -0500

    Update 6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
-orted_dictionary(a_dictionary):
-
-        for k in sorted(a_dictionary):
-
-                    print("{}: {}".format(k, a_dictionary[k]))
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for k in sorted(a_dictionary):
+        print("{}: {}".format(k, a_dictionary[k]))
+        
