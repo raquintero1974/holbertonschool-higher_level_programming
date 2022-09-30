8669cd4c48bce4acb3483e0ea06119050b793a61
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-30 12:44:01 -0500

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    data = {'M': 1000, 'D': 500, 'C': 100,
-            'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
-    prev, sum = 0, 0
-    for x in roman_string:
-        sum += data[x] if data[x] <= prev else data[x] - prev * 2
-        prev = data[x]
-    return sum
 
-                                                                                                                                                                                                                                                                                                                                                        flag = 1
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    total = 0
+    num = 0
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for r in reversed(roman_string):
+        num = digits[r]
+        total += num if total < num * 5 else -num
+    return total                                                                                                                                                                                                                                                                                                                              flag = 1
 
                                                                                                                                                                                                                                                                                                                                                                             elif roman_string[i] == 'X' and roman_string[i + 1] == 'C':
 
