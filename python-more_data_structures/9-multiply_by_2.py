9c0a869cfd41bbd4cefa8cd9caec793b8c59f0dd
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-30 10:20:34 -0500

    Update 9-multiply_by_2.py
    Fixing code

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
-_dictionary):
-        return {f: (i*2) for f, i in a_dictionary.items()}
-
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    return {f: (i*2) for f, i in a_dictionary.items()}
