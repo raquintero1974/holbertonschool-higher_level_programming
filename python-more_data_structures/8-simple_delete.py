a00bb2a239e7f929ff85b6270124a623edda72a3
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-30 09:28:38 -0500

    Update 8-simple_delete.py
    Fixing

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
-imple_delete(a_dictionary, key=""):
-
-        if key in a_dictionary:
-
-                    del a_dictionary[key]
-
-                        return a_dictionary
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
