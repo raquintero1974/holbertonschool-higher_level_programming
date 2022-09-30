f7b4cb6f60f0fa66da1b61edec7c6d61e108270a
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-09-30 08:53:58 -0500

    Update 2-uniq_add.py
    "Fixing code"

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
-
-
-def uniq_add(my_list-[]):
-    result = 0
-    for x in set(my_list):
-        result += x
-    return (result)
-
-
+def uniq_add(my_list=[]):
+        new_list = []
+        for i in my_list:
+            if i not in new_list:
+                new_list.append(i)
+        return sum(new_list)
