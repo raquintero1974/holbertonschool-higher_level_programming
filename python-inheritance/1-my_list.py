9deb35313712a65919ee71185d9cf42f7e1c2beb
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-10-22 18:34:53 -0500

    "Rewritting code"

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,11 @@
-fdfdfdfd
+#!/usr/bin/python3
+# 1-my_list.py
+"""Defines an inherited list class MyList."""
+
+
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
+
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
