babd0a5a24ac64fddad913718845863d11870a0e
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-10-23 12:53:35 -0500

    Update 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        return len(f.readlines())
+# 3-write_file.py
+""" File name : 3-write_file.py
+"""
+
+
+def write_file(filename="", text=""):
+    """write_file writes a string to a text file  (UTF8)
+    Args:
+        filename (str): Defaults to "".
+        text (str): text to add. Defaults to "".
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
