4c8c6d65525e7e40a86a82305c49f26bbdb5617b
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-11-16 23:09:20 -0500

    Update 15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,1 +1,6 @@
-jkj
+-- Script that lists the number of records with the same score in the table second_table.
+-- Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
