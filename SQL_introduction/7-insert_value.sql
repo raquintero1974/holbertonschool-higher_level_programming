0dea1762058b535018664f188aac5718a32bc2df
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-11-16 23:15:50 -0500

    Update 7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Script that inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- Script that inserts a new row in the table `first_table`
+-- in database `hbtn_0c_0` in MySQL Server.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School")
