43c9b5ab34ce5c30e3ef73e0ae533546a45ee43e
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-11-16 23:10:14 -0500

    Update 16-no_link.sql

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,1 +1,6 @@
-kjkl
+-- Script that lists all records of the table second_table having a name value.
+-- and records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
