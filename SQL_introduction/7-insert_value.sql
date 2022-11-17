75f430530d25c1b1c61dbf4e871f7952b1111b67
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-11-16 23:13:29 -0500

    Update 7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- This script inserts a new row into the table first_table.
+-- Script that inserts a new row into the table first_table.
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
