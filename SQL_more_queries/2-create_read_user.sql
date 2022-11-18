8db84b158047434061de19ae85d79ee259d77637
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-11-17 20:21:52 -0500

    Fixing  code

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,1 +1,7 @@
-re dkdkdk
+-- This script creates the database `hbtn_0d_2` and
+--  also the user `user_0d_2` with specific privileges.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
