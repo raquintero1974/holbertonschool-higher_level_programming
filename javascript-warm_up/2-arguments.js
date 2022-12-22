d96449b9678ffb1fb9a972a461ce9308e477d492
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-21 20:22:45 -0500

    Update 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-// Prints a predetermined messages depending of the number of args
-if (!process.argv[2]) {
-  console.log('No argument');
-} else if (process.argv[3]) {
-  console.log('Arguments found');
-} else {
-  console.log('Argument found');
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
