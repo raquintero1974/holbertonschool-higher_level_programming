866530f38f1bd5dd6454efb6cf1dd8e30be11c7b
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 16:38:27 -0500

    Update 9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,1 +1,8 @@
-oiuy
+#!/usr/bin/node
+//Casts two arguments into integers to be added
+
+let num1 = parseInt(process.argv[2]);
+let num2 = parseInt(process.argv[3]);
+let res = num1 + num2;
+
+console.log(res);
