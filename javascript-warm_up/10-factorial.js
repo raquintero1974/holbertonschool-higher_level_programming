cfb36041b202013093eb635b158ebd6a3f67c706
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 16:41:05 -0500

    Update 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
-hgdfd
+#!/usr/bin/node
+
+function factorial (num) {
+  if (isNaN(num) || num <= 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+let num = parseInt(process.argv[2]);
+
+console.log(factorial(num));
