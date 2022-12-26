a98a48606c959872b9a6d2b6bcc65fe4823dea1d
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 15:58:36 -0500

    Update 4-concat.js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,1 +1,4 @@
-ole.log(process.argv[2] + ' is ' + process.argv[3]);
+#!/usr/bin/node
+// Prints two arguments passed to it, in a given format
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);
