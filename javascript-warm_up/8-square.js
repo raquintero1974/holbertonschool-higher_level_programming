0f4bedc72908fd81b999a37ae2c1c298c43af709
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 16:34:49 -0500

    Update 8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
-uytr
+#!/usr/bin/node
+
+let num = parseInt(process.argv[2]);
+
+if (num) {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+} else if (num < 0) {
+  console.log('');
+} else {
+  console.log('Missing size');
+}
