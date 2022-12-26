5ef37c4a8410354fc30de5fd8520028ffc53d45c
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 16:42:22 -0500

    Update 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,16 @@
-hfgfg
+#!/usr/bin/node
+
+function sortNumber (a, b) {
+  return a - b;
+}
+let argsLen = process.argv.length;
+if (argsLen === 2 || argsLen === 3) {
+  console.log('0');
+} else {
+  let arr = [];
+  for (let i = 2; i < argsLen; i++) {
+    arr.push(process.argv[i]);
+  }
+  arr.sort(sortNumber);
+  console.log(arr[arr.length - 2]);
+}
