5c245534bc00c61221e96a55b31c96e92acaeeb1
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 12:49:30 -0500

    Delete javascript-warm_up directory

--- javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-// Making first print on JS!
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
--- javascript-warm_up/3-value_argument.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// Prints a predetermined messages depending of the number of args
-if (!process.argv[2]) {
-  console.log('No argument');
-} else if (process.argv[3]) {
-  console.log('Arguments found');
-} else {
-  console.log('Argument found');
-}
