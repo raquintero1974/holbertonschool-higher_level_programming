521a522a9f9268f8f121b495879154bb58d6370d
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 16:01:53 -0500

    Update 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,11 @@
-ggdgdgdg
+#!/usr/bin/node
+/* prints My number: <first argument converted in integer> 
+if the first argument can be converted to an integer*/
+
+let num = parseInt(process.argv[2]);
+
+if (num) {
+  console.log('My number: ' + num);
+} else {
+  console.log('Not a number');
+}
