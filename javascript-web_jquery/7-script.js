b8e68e0282471d077b325c78fd6a0022243fb9b8
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 22:09:08 -0500

    Update 7-script.js

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,1 +1,8 @@
-jkljaj
+$.ajax({
+  url: 'https://swapi.co/api/people/5/?format=json',
+  type: 'GET',
+  dataType: 'json'
+})
+  .done(function (json) {
+    $('DIV#character').text(json.name);
+  });
