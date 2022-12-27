d838826bfde14c653c4d60bab2a39732df551914
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 20:32:39 -0500

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-// Defines a rectangle
+
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0 {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
