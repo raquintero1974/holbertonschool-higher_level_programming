96eae9000335a5bc199b28638152fb56d691fdab
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 20:57:33 -0500

    Update 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
-gf
+#!/usr/bin/node
+
+let fs = require('fs');
+let argstr = process.argv[3];
+fs.writeFile(process.argv[2], argstr, function (err, argstr) {
+  if (err) {
+    console.log(err);
+  }
+});
