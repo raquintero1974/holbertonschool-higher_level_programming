7cf4c595144a994e1eeb3983ff7225b78397d59b
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 20:58:27 -0500

    Update 2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,9 @@
-hhh
+#!/usr/bin/node
+
+require('request').get(process.argv[2], function (err, r) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + r.statusCode);
+  }
+});
