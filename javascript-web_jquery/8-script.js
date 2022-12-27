d3b7c47771c5225c613f92324933a9a0bba3145a
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 22:10:30 -0500

    Update 8-script.js

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,1 +1,10 @@
-hg
+$.ajax({
+  url: 'https://swapi.co/api/films/?format=json',
+  type: 'GET',
+  dataType: 'json'
+})
+  .done(function (json) {
+    for (const results of json.results) {
+      $('UL#list_movies').append('<li>' + results.title + '</li>');
+    }
+  });
