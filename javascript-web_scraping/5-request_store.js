1fb863b45f04e7bebf6142dbda9f4337a174496e
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 21:10:59 -0500

    Update 5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-uyuyu;
-=======
 #!/usr/bin/node
 
 require('request').get(process.argv[2], function (err, r, body) {
@@ -14,4 +11,3 @@ require('request').get(process.argv[2], function (err, r, body) {
     });
   }
 });
->>>>>>> 854a928eb1b0c9c71f2e364b94811be6361c969e
