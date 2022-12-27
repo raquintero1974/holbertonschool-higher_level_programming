a956c794fae82e98504831667696e8afaa1a7184
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 20:35:55 -0500

    Update 6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,13 @@
-fdfdf
+#!/usr/bin/node
+
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
