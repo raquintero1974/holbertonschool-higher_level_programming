c7899049a0a69377ad89592661a256a56b10d3a9
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 21:15:12 -0500

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-tt;
-=======
 #!/usr/bin/node
 
 let fs = require('fs');
@@ -11,4 +8,3 @@ fs.readFile(process.argv[2], function (err, contents) {
     console.log(err);
   }
 });
->>>>>>> 854a928eb1b0c9c71f2e364b94811be6361c969e
