89e8ffa7474ff56e8ab972447a5239f239aedba6
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 21:12:42 -0500

    Update 3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-uyt;
-=======
+
 #!/usr/bin/node
 
 require('request').get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, r, body) {
@@ -10,4 +8,3 @@ require('request').get('http://swapi.co/api/films/' + process.argv[2] + '/', fun
     console.log(JSON.parse(body).title);
   }
 });
->>>>>>> 854a928eb1b0c9c71f2e364b94811be6361c969e
