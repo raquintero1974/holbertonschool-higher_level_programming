d793f42861240eaed7318774db319e9e5949e596
Raúl <98792320+raquintero1974@users.noreply.github.com>
2022-12-26 21:09:38 -0500

    Update 6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-6;
-=======
 #!/usr/bin/node
 
 require('request').get(process.argv[2], function (err, r, body) {
@@ -20,4 +17,4 @@ require('request').get(process.argv[2], function (err, r, body) {
     console.log(res);
   }
 });
->>>>>>> 854a928eb1b0c9c71f2e364b94811be6361c969e
+
